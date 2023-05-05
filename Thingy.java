c8ba29e92bf5b774da2835c2dfba72a8faed4cde
KeaganMorrison <114590658+KeaganMorrison@users.noreply.github.com>
2023-05-05 09:18:00 -0400

    Update Thingy.java

--- Thingy.java
+++ Thingy.java
@@ -8,21 +8,26 @@ public class Thingy extends Item {
     Random rando = new Random();
     int doodle = rando.nextInt(1,2);
     
-    public static void setSale(int doodle, int Cost, Random rando, int week, ArrayList<Item> Wares, int i, ArrayList<Item> Multipliers ){
+    @Override
+    public  void setSale( Random rando, ArrayList<Item> Wares, ArrayList<Item> Multipliers ){
+      int doodle = rando.nextInt(1,3);
       int d = rando.nextInt(0, Wares.size());
       int c = rando.nextInt(0, Multipliers.size());  
       
       if ( doodle == 1 ) {
         Wares.get(d).Cost /= 2;
         Multipliers.get(c).Cost/=2;
+        System.out.println("Special Action sale on " + Wares.get(d).title + " and " + Multipliers.get(c).title + "!");
+        System.out.println(" ");
+        
       }
       if (doodle==2) {
         Wares.get(d).Cost*=2;
         Multipliers.get(c).Cost*=2;
+        System.out.println("Wuh oh, the price of the Action  " + Wares.get(d).title + " and " + Multipliers.get(c).title + " went way up!");
+        System.out.println(" ");
       }
-      else {
-        setSale(doodle, Cost, rando, week, Wares, i, Multipliers);
-      }
+     
     }
 
   }
